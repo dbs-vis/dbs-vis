cbfde54db4c334b9154d2baf8937b217b59c3eb2
Johannes Graupe <johannes.graupe@mail.de>
2020-08-09 00:47:08 +0200

    Show <title> correctlyand reorganise links in header and footer

--- requires/footer.php
+++ requires/footer.php
@@ -1,31 +1,11 @@
 		</main>
-		<?php
-			$pages = [
-				[
-					'name' => 'stop',
-					'linktext' => 'Kontaktformular',
-				],
-				[
-					'name' => 'impressum',
-					'linktext' => 'Impressum',
-				],
-			];
-
-			foreach ($pages as $index => $page) :
-			  $listitem = "<li";
-			  if ($_SERVER["SCRIPT_NAME"] == "/dbs-vis/" . $page["name"] . ".php") :
-				$listitem .= " aria-current='page'><a>";
-			  else :
-				$listitem .= "><a href='" . $page["name"] . ".php'>";
-			  endif;
-			  $listitem .= $page["linktext"] . "</a></li>";
-			  $pages[$index]["listitem"] = $listitem;
-			endforeach;
-		?>
 		<footer>
 			<ul>
 				<?php
-					foreach ($pages as $page) : echo $page["listitem"];
+					foreach ($pages as $page) :
+						if ($page["place"] == "footer"):
+							echo $page["listitem"];
+						endif;
 					endforeach;
 				?>
 				<li>Stand: 06.08.2020</li>
--- requires/header.php
+++ requires/header.php
@@ -1,7 +1,32 @@
 <!DOCTYPE html>
 <html lang="de">
+<?php
+	$pages = [
+		['name' => 'search',	'linktext' => 'Suche',				'place' => 'header' ],
+		['name' => 'stop',		'linktext' => 'Anleitung',			'place' => 'header' ],
+		['name' => 'stop',		'linktext' => 'Hintergrund',		'place' => 'header' ],
+		['name' => 'stop',		'linktext' => 'Blog',				'place' => 'header' ],
+		['name' => 'stop',		'linktext' => 'Konto',				'place' => 'header'],
+		['name' => 'stop',		'linktext' => 'Kontaktformular',	'place' => 'footer'],
+		['name' => 'impressum',	'linktext' => 'Impressum',			'place' => 'footer'],
+		['name' => 'data',		'linktext' => 'Daten',				'place' => 'nowhere'],
+	];
+
+	$title = "Startseite - DBS-VIS";
+	foreach ($pages as $index => $page) :
+		$listitem = "<li";
+		if ($_SERVER["SCRIPT_NAME"] == "/dbs-vis/" . $page["name"] . ".php") :
+			$listitem .= " aria-current='page'><a>";
+			$title = $page["linktext"] . " - DBS-VIS";
+		else :
+			$listitem .= "><a href='" . $page["name"] . ".php'>";
+		endif;
+		$listitem .= $page["linktext"] . "</a></li>";
+		$pages[$index]["listitem"] = $listitem;	
+	endforeach;
+?>
 	<head>
-		<title>Startseite - dbs-vis</title>
+		<title><?php echo $title; ?></title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="stylesheet" href="./stylesheet.css">
@@ -12,49 +37,17 @@
 			<div id="headline">
 				<a href="./index.php" class="head">
 					<img id="logo" alt="" src="graphics/logo.svg">
-					<h1>DBS-Vis</h1>
+					<h1>DBS-VIS</h1>
 				</a>
 			</div>
 		</header>
-		<?php
-			$pages = [
-				[
-					'name' => 'search',
-					'linktext' => 'Suche',
-				],
-				[
-					'name' => 'stop',
-					'linktext' => 'Anleitung',
-				],
-				[
-					'name' => 'stop',
-					'linktext' => 'Hintergrund',
-				],
-				[
-					'name' => 'stop',
-					'linktext' => 'Blog',
-				],
-				[
-					'name' => 'stop',
-					'linktext' => 'Konto',
-				],
-			];
-
-			foreach ($pages as $index => $page) :
-			  $listitem = "<li";
-			  if ($_SERVER["SCRIPT_NAME"] == "/dbs-vis/" . $page["name"] . ".php") :
-				$listitem .= " aria-current='page'><a>";
-			  else :
-				$listitem .= "><a href='" . $page["name"] . ".php'>";
-			  endif;
-			  $listitem .= $page["linktext"] . "</a></li>";
-			  $pages[$index]["listitem"] = $listitem;
-			endforeach;
-		?>
 		<nav>
 			<ul>
 				<?php
-					foreach ($pages as $page) : echo $page["listitem"];
+					foreach ($pages as $page) :
+						if ($page["place"] == "header"):
+							echo $page["listitem"];
+						endif;
 					endforeach;
 				?>
 			</ul>
