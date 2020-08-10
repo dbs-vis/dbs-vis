af04a4facca4b2aaee26e03d106880b6cd112276
Johannes Graupe <johannes.graupe@mail.de>
2020-08-10 10:04:46 +0200

    Create background.php

---
+++ background.php
@@ -0,0 +1,20 @@
+<?php
+	require './requires/header.php';
+?>
+		</div>
+		<main>
+			<article>
+				<header>
+					<h2 id="history">Geschichte</h2>
+				</header>
+				<p>DBS-VIS entstand während des Sommersemesters 2020 als Teil der Masterarbeit "Ein Datencockpit für wissenschaftliche Bibliotheken - Präsentation von Daten der Deutschen Bibliotheksstatistik" im Studiengang <a href="https://www.th-koeln.de/studium/bibliotheks--und-informationswissenschaft-master_3202.php">Bibliotheks- und Informationswissenschaft / MALIS (Master in Library and Information Science)</a> an der <a href="https://www.th-koeln.de/startseite_16.php">TH Köln</a> durch <a href="https://orcid.org/0000-0002-4818-7324">Johannes Graupe</a>. Betreut wurde die Arbeit durch <a href="https://www.th-koeln.de/personen/simone.fuehles-ubach/">Prof. Dr. Simone Fühles-Ubach</a> und <a href="https://www.th-koeln.de/personen/konrad.foerstner/">Prof. Dr. Konrad Förstner</a>. Der Quellcode wurde dabei im GitHub-Repositorium <a href="https://github.com/jgraupe/dbs-vis">dbs-vis</a> unter der <a href="https://opensource.org/licenses/isc-license.txt">ISC-Lizenz</a> veröffentlicht.</p>
+			</article>
+			<article>
+				<header>
+					<h2 id="publications">Veröffentlichungen</h2>
+				</header>
+				<p>Hier werden Sie zukünftig alle Publikationen finden, in denen DBS-VIS eine Rolle spielt.</p>
+			</article>
+<?php
+	require './requires/footer.php';
+?>		
