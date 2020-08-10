6210b04a15a657b63b23dad34685660e01d7e617
Johannes Graupe <johannes.graupe@mail.de>
2020-08-10 10:23:52 +0200

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,13 @@
+<?php
+	require './requires/header.php';
+?>
+
+		</div>
+		<main>
+			<header>
+				<h2>Achtung, Baustelle!</h2>
+			</header>
+			Bitte entschuldigen Sie. Diese Seite existiert noch nicht.
+<?php
+	require './requires/footer.php';
+?>		
