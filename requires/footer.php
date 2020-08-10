ab41d2c892ad18c6320eefc6658aef7b66dbbf51
Johannes Graupe <johannes.graupe@mail.de>
2020-08-11 00:25:23 +0200

    Change date to alpha release date

--- requires/footer.php
+++ requires/footer.php
@@ -8,7 +8,7 @@
 						endif;
 					endforeach;
 				?>
-				<li>Stand: 06.08.2020</li>
+				<li>Stand: 11.08.2020</li>
 				<li><a href="https://jigsaw.w3.org/css-validator/check/referer"><img style="border:0;width:88px;height:31px" src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS ist valide!" /></a></li>
 			</ul>
 		</footer>
