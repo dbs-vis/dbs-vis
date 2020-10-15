ed6dbddf896c4636e84b9d1ada581a06464bc19a
Johannes Graupe <johannes.graupe@mail.de>
2020-10-16 00:28:26 +0200

    Change date

--- requires/footer.php
+++ requires/footer.php
@@ -7,8 +7,12 @@
 						};
 					};
 				?>
-				<li>Stand: 15.10.2020</li>
-				<li><a href="https://jigsaw.w3.org/css-validator/check/referer"><img style="border:0;width:88px;height:31px" src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS ist valide!" /></a></li>
+				<li>Stand: 16.10.2020</li>
+				<li>
+					<a href="https://jigsaw.w3.org/css-validator/check/referer">
+						<img style="border:0;width:88px;height:31px" src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS ist valide!" />
+					</a>
+				</li>
 			</ul>
 		</footer>
 	</body>
