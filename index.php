ab7a45bf77aa832acfe8cd126aaf6a9af37d2d96
Johannes Graupe <johannes.graupe@mail.de>
2020-10-16 00:00:30 +0200

    Update links

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 						Bisher sind die Daten wissenschaftlicher Bibliotheken der Berichtsjahre 2018 und 2019 aus Deutschland und Österreich verfügbar.
 					</strong>
 				</p>
-				<p>DBS-Vis (Deutsche Bibliotheksstatistik-Visualisierung) ist ein Projekt zur grafischen Darstellung bibliotheksstatistischer Daten. Die Daten der einzelnen Bibliotheken können in der <a href="stop.html">Statistik</a> über eine Landkarte oder die auch hier angebotene Suche gefunden werden. Die Benutzung der verschiedenen Anwendungsmöglichkeiten wird in der <a href="stop.html">Anleitung</a> ausführlich beschrieben. Auch der <a href="stop.html">Hintergrund</a> der Website wird dargestellt. In unserem <a href="stop.html">Blog</a> erfahren Sie alle Neuigkeiten zu DBS-Vis. Mit einem <a href="stop.html">Konto</a> ist es möglich, benutzerdefinierte Visualisierungen schnell und einfach zu erstellen und aufzurufen.</p>
+				<p>DBS-Vis (Deutsche Bibliotheksstatistik-Visualisierung) ist ein Projekt zur grafischen Darstellung bibliotheksstatistischer Daten. Die Daten der einzelnen Bibliotheken können in der <a href="./search.php">Suche</a> z. B. über eine Landkarte oder das oben angebotene Suchfenster gefunden werden. Die Benutzung der verschiedenen Anwendungsmöglichkeiten wird in der <a href="./guidance.php">Anleitung</a> ausführlich beschrieben. Auch der <a href="./background.php">Hintergrund</a> der Website wird dargestellt. In unserem <a href="./blog.php">Blog</a> erfahren Sie alle Neuigkeiten zu DBS-Vis. Mit einem <a href="./login.php">Konto</a> ist es möglich, benutzerdefinierte Visualisierungen schnell und einfach zu erstellen und aufzurufen.</p>
 			</article>
 			<article>
 				<header>
