2caaafdfd016822b458f112db3d0c21822176bf5
Johannes Graupe <johannes.graupe@mail.de>
2020-10-19 23:28:48 +0200

    Move CSS commands into stylesheet.css

--- requires/footer.php
+++ requires/footer.php
@@ -10,7 +10,7 @@
 				<li>Stand: 19.10.2020</li>
 				<li>
 					<a href="https://jigsaw.w3.org/css-validator/check/referer">
-						<img style="border:0;width:88px;height:31px" src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS ist valide!" />
+						<img id="css-validator" src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS ist valide!" />
 					</a>
 				</li>
 			</ul>
